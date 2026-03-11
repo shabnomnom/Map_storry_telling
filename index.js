046e366da66dae901dd83faedb63b1ba611cd64e
shabnomnom <shabnam.shomail@gmail.com>
2026-03-11 13:02:18 -0400

    first draft at basic chat assistant using Anthropic sdk

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 import Anthropic from "@anthropic-ai/sdk";
+import { createInterface } from "readline/promises";
+
+const rl = createInterface({ input: process.stdin, output: process.stdout });
+const prompt = (q = "") => rl.question(q);
 
 const client = new Anthropic({
   apiKey: process.env.Claude_KEY,
 });
 
-const message = await client.messages.create({
-  model: "claude-sonnet-4-5-20250929",
-  max_tokens: 1000,
-  messages: [{ role: "user", content: "Hello, Claude!" }],
-});
+function add_user_message(messages, text) {
+  let user_message = { role: "user", content: text };
+  messages.push(user_message);
+}
 
-console.log(message.content[0].text);
+function add_assistant_message(messages, content) {
+  let assistant_message = { role: "assistant", content };
+  messages.push(assistant_message);
+}
+
+// make a request to the API
+async function chat(messages) {
+  const message = await client.messages.create({
+    model: "claude-sonnet-4-5-20250929",
+    max_tokens: 1000,
+    messages: messages,
+  });
+  //   console.log(message.content[0].text);
+  return message.content[0].text;
+}
+
+// create a while True loop to keep the conversation going until the user types "exit"
+const messages = [];
+while (true) {
+  let user_input = await prompt(">");
+  if (user_input.toLowerCase() === "exit") {
+    rl.close();
+    break;
+  }
+  add_user_message(messages, user_input);
+  const answer = await chat(messages);
+  console.log(answer);
+  console.log(messages);
+  add_assistant_message(messages, answer);
+}
